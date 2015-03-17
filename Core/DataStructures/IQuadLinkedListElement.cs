efd275a7a7f399fdaf2c41c86b359a492c4a4c69
Daniel Charles <p.danielcharles@gmail.com>
2015-03-16 20:14:38 -0700

    Fixed reference to Core project from Test project and added missing interface from last commit.

---
+++ Core/DataStructures/IQuadLinkedListElement.cs
@@ -0,0 +1,18 @@
+﻿using Functional.Option;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataStructures
+{
+    public interface IQuadLinkedListElement<T>
+    {
+        T Value { get; }
+        Option<IQuadLinkedListElement<T>> North { get; }
+        Option<IQuadLinkedListElement<T>> East { get; }
+        Option<IQuadLinkedListElement<T>> South { get; }
+        Option<IQuadLinkedListElement<T>> West { get; }
+    }
+}
